1f5039dd5b36c4308825e1c75039c287ef002ce8
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-08 05:26:48 +0300

    chenged styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,26 +29,6 @@ export class AppComponent {
       new Date('2022,02,06')
 
     ),
-    new Quote(
-      Quote.generateId(),
-      "LordKinyua",
-      "Success isn`t the key to happiness but happiness is the key to success",
-      "THE DON",
-      0,
-      0,
-      new Date('2022,02,06')
-
-    ),
-    
-    new Quote(
-      Quote.generateId(),
-      "LordKinyua",
-      "Some people are alive but aren`t living",
-      "THE DON",
-      0,
-      0,
-      new Date('2022,02,03')
-      ),
   
 
 
